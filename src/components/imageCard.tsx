6001e0e4364f45b1fcd6cadd884c029c89998a6c
Vishnu Kumar <vishnukumart96@gmail.com>
2023-12-29 11:27:35 +0530

    edit image Card

--- src/components/imageCard.tsx
+++ src/components/imageCard.tsx
@@ -1,12 +1,22 @@
 export function ImageCard(){
 
     return(
-        <div className="border h-[350px] w-[400px] overflow-hidden rounded-3xl object-cover shadow-slate-600 shadow-2xl mt-10 dark:shadow-slate-400 dark:animate-pulse">
-            <img 
-                src ={`https://c1.wallpaperflare.com/preview/625/985/142/integrated-circuit-device-chip-technology.jpg`}
-                alt='design-integrated-circuit'
-                className="h-full w-full "
-            />
+        <div>
+
+
+
+            <div className="border h-[350px] w-[400px] overflow-hidden rounded-3xl object-cover shadow-slate-600 shadow-2xl mt-10 dark:shadow-slate-400 dark:animate-pulse">
+                <img 
+                    src ={`https://c1.wallpaperflare.com/preview/625/985/142/integrated-circuit-device-chip-technology.jpg`}
+                    alt='design-integrated-circuit'
+                    className="h-full w-full "
+                />
+            </div>
+
+            <blockquote className="mt-10 pl-6 italic text-center">
+                "Meet the Future"
+            </blockquote>
         </div>
+        
     )
 }
