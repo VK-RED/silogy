f11673547cf7233144b41fb8bcee24420dabb698
Vishnu Kumar <vishnukumart96@gmail.com>
2023-12-29 11:27:01 +0530

    add links

--- constants.ts
+++ constants.ts
@@ -1,3 +1,7 @@
 export const TITLE = `SiLogy`;
 export const MAIN_DESCRIPTION = `Meet SiLogy, The Next generation tool for Integrated Circuit Verification`;
 export const BRIEF_DESCRIPTION = `Harness the power of advanced technology, we propel the industry forward, empowering businesses to expedite their go-to-market strategies`
+export const GMAIL_LINK =  `https://www.gmail.com`
+export const GITHUB_LINK = `https://www.github.com/VK-RED`
+export const PROJECT_LINK = `https://www.github.com/VK-RED/silogy`
+export const TWITTER_LINK = `https://twitter.com/focusedVK`
