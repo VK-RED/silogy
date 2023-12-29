3812dd6dd1fe28206c0cf6692dd285f0c50532e9
Vishnu Kumar <vishnukumart96@gmail.com>
2023-12-29 07:57:31 +0530

    add dark mode suppor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { DarkModeToggle } from '@/components/darkModeToggle'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
-      HI I am the Home Page !!!
+      <div className='mt-2'>
+        <DarkModeToggle />
+      </div>
+    
     </div>
   )
 }
