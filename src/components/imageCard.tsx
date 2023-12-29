011bf6bccb9d7b2aa493b84f1ea576b8951bc5b1
Vishnu Kumar <vishnukumart96@gmail.com>
2023-12-29 12:45:57 +0530

    bring image natively

--- src/components/imageCard.tsx
+++ src/components/imageCard.tsx
@@ -2,12 +2,9 @@ export function ImageCard(){
 
     return(
         <div>
-
-
-
             <div className="border h-[350px] w-[400px] overflow-hidden rounded-3xl object-cover shadow-slate-600 shadow-2xl mt-10 dark:shadow-slate-400 dark:animate-pulse">
                 <img 
-                    src ={`https://c1.wallpaperflare.com/preview/625/985/142/integrated-circuit-device-chip-technology.jpg`}
+                    src ={`ic.jpg`}
                     alt='design-integrated-circuit'
                     className="h-full w-full "
                 />
