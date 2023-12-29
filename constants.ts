798533204c11387fed11711188130dfcc6aa9278
Vishnu Kumar <vishnukumart96@gmail.com>
2023-12-29 09:03:57 +0530

    add constants

---
+++ constants.ts
@@ -0,0 +1,3 @@
+export const TITLE = `SiLogy`;
+export const MAIN_DESCRIPTION = `Meet SiLogy, The Next generation tool for Integrated Circuit Verification`;
+export const BRIEF_DESCRIPTION = `Harness the power of advanced technology, we propel the industry forward, empowering businesses to expedite their go-to-market strategies`
