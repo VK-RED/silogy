aba5b109753d5424959689702857287d9c221a12
Vishnu Kumar <vishnukumart96@gmail.com>
2023-12-29 12:26:11 +0530

    add title in tab Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,16 @@ import { TITLE } from '../../constants'
 import { ImageCard } from '@/components/imageCard'
 import { WaitListCard } from '@/components/waitListCard'
 import { Footer } from '@/components/footer'
+import Head from 'next/head'
+
+
 
 export default function Home() {
   return (
     <div className='h-screen flex flex-col items-center py-5 min-w-[500px] w-full'>
-
+        <Head>
+          <title>{TITLE}</title>
+        </Head>
         <div className='w-full flex justify-between py-2 items-center'>
             <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl ml-20">
               {TITLE}
@@ -18,7 +23,7 @@ export default function Home() {
             </div>
         </div>
 
-        {/* <ImageCard /> */}
+        
         <Hero />
 
         <div className='flex flex-col md:flex-row md:space-x-7 lg:space-x-14 justify-between items-center'>
