b5b648daa172c8cc16c126b4de8ad8647348d848
Vishnu Kumar <vishnukumart96@gmail.com>
2023-12-29 09:04:17 +0530

    add Hero Section

---
+++ src/components/hero.tsx
@@ -0,0 +1,17 @@
+import { BRIEF_DESCRIPTION, MAIN_DESCRIPTION } from "../../constants";
+
+export function Hero(){
+    return (
+        <div className="mt-20  p-2 flex flex-col items-center space-y-2">
+
+            <h3 className="scroll-m-20 text-2xl font-medium tracking-tight text-zinc-600 dark:text-zinc-400">
+                {MAIN_DESCRIPTION}
+            </h3>
+
+            <h4 className="leading-7 [&:not(:first-child)]:mt-6 text-zinc-500">
+                {BRIEF_DESCRIPTION}
+            </h4>
+            
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import { DarkModeToggle } from '@/components/darkModeToggle'
+import { Hero } from '@/components/hero'
+import { TITLE } from '../../constants'
 
 
 export default function Home() {
   return (
-    <div>
-      <div className='mt-2'>
-        <DarkModeToggle />
-      </div>
+    <div className='h-screen border border-red-700 flex flex-col items-center py-5 min-w-[500px]'>
+
+        <div className='w-full flex justify-between py-2 items-center'>
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl ml-20">
+              {TITLE}
+            </h1>
+            <div className='mr-10'>
+                <DarkModeToggle />
+            </div>
+        </div>
+        
+
+        <Hero />
     
     </div>
   )
